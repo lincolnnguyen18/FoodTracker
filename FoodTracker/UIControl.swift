180579ccc2fa9d8f55a6be746ab73aad6e5e6c62
Lincoln Nguyen <letsmakeaworld@icloud.com>
2021-01-29 08:02:18 -0600

    Forgot to start practice committing.

---
+++ FoodTracker/UIControl.swift
@@ -0,0 +1,29 @@
+//
+//  UIControl.swift
+//  FoodTracker
+//
+//  Created by Lincoln Nguyen on 1/29/21.
+//
+
+import UIKit
+
+class UIControl: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
